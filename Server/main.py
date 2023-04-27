93a113d6a9932b9db42ebb21206ab797be3370c6
NikshetSteh <88205797+NikshetSteh@users.noreply.github.com>
2023-04-27 17:54:06 +0500

    Create test flask app

--- Server/main.py
+++ Server/main.py
@@ -1,2 +1,12 @@
-print('Test')
+from flask import Flask, request, Response
 
+app = Flask(__name__)
+
+
+@app.route("/test/echo", methods=['POST', 'GET'])
+def test_echo() -> Response:
+    return Response(request.data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
