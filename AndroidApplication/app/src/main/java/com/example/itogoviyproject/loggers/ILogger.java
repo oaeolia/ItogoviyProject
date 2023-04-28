768b1fffc99b4bb832f346e009e4d900b8c6366b
NikshetSteh <88205797+NikshetSteh@users.noreply.github.com>
2023-04-28 18:53:13 +0500

    [Client] Added document comment for the ILogger class

--- AndroidApplication/app/src/main/java/com/example/itogoviyproject/loggers/ILogger.java
+++ AndroidApplication/app/src/main/java/com/example/itogoviyproject/loggers/ILogger.java
@@ -1,7 +1,14 @@
 package com.example.itogoviyproject.loggers;
 
+/*
+This class abstracts the general interface for the logging class. This is necessary because
+different types of logging can be useful in different situations. The usual console for debugging
+the program or sending to the server for further analysis and processing
+ */
 public interface ILogger {
     public void logInfo(String tag, String message);
+
     public void logWarming(String tag, String message);
+
     public void logError(String tag, String message);
 }
