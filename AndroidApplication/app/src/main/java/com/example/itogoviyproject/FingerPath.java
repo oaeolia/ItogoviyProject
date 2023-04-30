6bb58e7e6f668d71953d2875c7ce19e494f93863
oaeolia <olya.mart07@yandex.ru>
2023-04-30 13:23:16 +0500

    разметка пока хромает и не все кнопки нормально функционируют

--- AndroidApplication/app/src/main/java/com/example/itogoviyproject/FingerPath.java
+++ AndroidApplication/app/src/main/java/com/example/itogoviyproject/FingerPath.java
@@ -2,14 +2,10 @@ package com.example.itogoviyproject;
 import android.graphics.Path;
 public class FingerPath {
     public int color;
-    public boolean emboss;
-    public boolean blur;
     public int strokeWidth;
     public Path path;
-    public FingerPath(int color, boolean emboss, boolean blur, int strokeWidth, Path path) {
+    public FingerPath(int color, int strokeWidth, Path path) {
         this.color = color;
-        this.emboss = emboss;
-        this.blur = blur;
         this.strokeWidth = strokeWidth;
         this.path = path;
     }
--- AndroidApplication/app/src/main/java/com/example/itogoviyproject/MainActivity.java
+++ AndroidApplication/app/src/main/java/com/example/itogoviyproject/MainActivity.java
@@ -3,23 +3,40 @@ package com.example.itogoviyproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton currentPaint;
     private PaintView paintView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
-        paintView = findViewById(R.id.paintView);
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        paintView.init(metrics);
+        paintView = (PaintView)findViewById(R.id.paintView);
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+        currentPaint = (ImageButton)paintLayout.getChildAt(0);
+        currentPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
     }
+
+    public void paintClicked(View view){
+        if(view!= currentPaint){
+            ImageButton imgView = (ImageButton)view;
+            String color = view.getTag().toString();
+            paintView.setColor(color);
+            imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+            currentPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
+            currentPaint =(ImageButton)view;
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -38,46 +55,48 @@ public class MainActivity extends AppCompatActivity {
             ((Application) getApplication()).getServer().logout();
             return true;
         }
-
-        switch(item.getItemId()) {
-            case R.id.normal:
-                paintView.normal();
-                return true;
-            case R.id.emboss:
-                paintView.emboss();
-                return true;
-            case R.id.blur:
-                paintView.blur();
-                return true;
-            case R.id.clear:
-                paintView.clear();
-                return true;
-            case R.id.size_small:
-                paintView.size_small();
-                return true;
-            case R.id.size_normal:
-                paintView.size_normal();
-                return true;
-            case R.id.size_big:
-                paintView.size_big();
-                return true;
-            case R.id.color_green:
-                paintView.color_green();
-                return true;
-            case R.id.color_red:
-                paintView.color_red();
-                return true;
-            case R.id.color_black:
-                paintView.color_black();
-                return true;
-            case R.id.color_yellow:
-                paintView.color_yellow();
-                return true;
-            case R.id.color_blue:
-                paintView.color_blue();
-                return true;
-        }
-
+//        switch(item.getItemId()) {
+//            case R.id.normal:
+//                paintView.normal();
+//                return true;
+//            case R.id.emboss:
+//                paintView.emboss();
+//                return true;
+//            case R.id.blur:
+//                paintView.blur();
+//                return true;
+//            case R.id.clear:
+//                paintView.clear();
+//                return true;
+//            case R.id.size_small:
+//                paintView.size_small();
+//                return true;
+//            case R.id.size_normal:
+//                paintView.size_normal();
+//                return true;
+//            case R.id.size_big:
+//                paintView.size_big();
+//                return true;
+//            case R.id.color_green:
+//                paintView.color_green();
+//                return true;
+//            case R.id.color_red:
+//                paintView.color_red();
+//                return true;
+//            case R.id.color_black:
+//                paintView.color_black();
+//                return true;
+//            case R.id.color_yellow:
+//                paintView.color_yellow();
+//                return true;
+//            case R.id.color_blue:
+//                paintView.color_blue();
+//                return true;
+//            case R.id.color_pink:
+//                paintView.color_pink();
+//                return true;
+//
+//        }
         return super.onOptionsItemSelected(item);
     }
 }
