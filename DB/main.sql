5eb6c2b2c1e4abba38c4a503f5abae6002d904ee
NikshetSteh <88205797+NikshetSteh@users.noreply.github.com>
2023-05-03 14:41:51 +0500

    [DB] Added game rooms table

--- DB/main.sql
+++ DB/main.sql
@@ -27,3 +27,34 @@ CREATE TABLE `application_sessions`
     PRIMARY KEY (`id`),
     FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
 );
+
+CREATE TABLE `games_rooms`
+(
+    `id`                 INT UNSIGNED                                                   NOT NULL AUTO_INCREMENT,
+    `now_word`           VARCHAR(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    `is_started`         BOOLEAN                                                        NOT NULL DEFAULT 0,
+    `user_1`             INT UNSIGNED,
+    `user_2`             INT UNSIGNED,
+    `user_3`             INT UNSIGNED,
+    `user_4`             INT UNSIGNED,
+    `user_5`             INT UNSIGNED,
+    `checked_user_1`     boolean                                                        NOT NULL DEFAULT 0,
+    `checked_user_2`     boolean                                                        NOT NULL DEFAULT 0,
+    `checked_user_3`     boolean                                                        NOT NULL DEFAULT 0,
+    `checked_user_4`     boolean                                                        NOT NULL DEFAULT 0,
+    `checked_user_5`     boolean                                                        NOT NULL DEFAULT 0,
+    `start_checked_time` DATETIME,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_1`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`user_2`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`user_3`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`user_4`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`user_5`) REFERENCES `users` (`id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `words`
+(
+    `id`       INT UNSIGNED                                                   NOT NULL AUTO_INCREMENT,
+    `word` VARCHAR(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    PRIMARY KEY (`id`)
+)
